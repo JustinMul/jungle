b2209cab44d6c87fa01f6e1d8bf5f86bea34f316
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-04-12 15:20:58 -0400

    add testing for product details page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-  # SETUP
+RSpec.feature "ProductDetails", type: :feature do
+
+
   before :each do
 
     @category = Category.create! name: 'Apparel'
@@ -17,13 +18,13 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-   scenario "They see all products" do
+   scenario "Navigate from the home page to the product detail page" do
     visit root_path
+    first('article.product').click_on("Details")
     
-    # commented out b/c it's for debugging only
-    save_screenshot
+    # # commented out b/c it's for debugging only
+    # save_screenshot
 
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css 'section.products-show', count: 1
   end
-
 end
